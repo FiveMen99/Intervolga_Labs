853fecff39c6e8bcc5525f5b7d34bbd116b0f3ab
Nikita <nikita062499@gmail.com>
2019-10-20 22:05:30 +0400

    Update func.php

--- func.php
+++ func.php
@@ -1,8 +1,8 @@
 <?php
 function safety_request ($a){
-$a=htmlspecialchars($a);
+$a=htmlspecialchars($a);//можно было добавить real_escape_string (и функция была бы немного больше), но в лекции сказано делать по другому
 return $a;;	
-				}
+	}
 
 
 
