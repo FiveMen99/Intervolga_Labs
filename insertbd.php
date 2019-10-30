7ace9f28e720343748ea13dee63885952dbd5b8c
Nikita <nikita062499@gmail.com>
2019-10-30 21:14:33 +0400

    изменил названия переменных+ добавил к валидации php регулярку

--- insertbd.php
+++ insertbd.php
@@ -2,36 +2,38 @@
 <?php
 include("bd.php");
 include("func.php");
-$postdannyy=@$_POST;
-$postdannyy['date']=safety_request($postdannyy['date']);
-$postdannyy['matematics']=safety_request($postdannyy['matematics']);
-$postdannyy['russich']=safety_request($postdannyy['russich']);
-$postdannyy['history']=safety_request($postdannyy['history']);
-	$dlinadata=strlen($postdannyy['date']);
-	$dlinamatan=strlen($postdannyy['matematics']);
-	$dlinaruss=strlen($postdannyy['russich']);
-	$dlinahist=strlen($postdannyy['history']);
-		
+$postinformation=@$_POST;
+$postinformation['date']=safety_request($postinformation['date']);
+$postinformation['matematics']=safety_request($postinformation['matematics']);
+$postinformation['russich']=safety_request($postinformation['russich']);
+$postinformation['history']=safety_request($postinformation['history']);
+	$lengthdata=strlen($postinformation['date']);
+	$lengthmatan=strlen($postinformation['matematics']);
+	$lengthruss=strlen($postinformation['russich']);
+	$lengthhist=strlen($postinformation['history']);
+	$regular1='/([01-9])+\.([01-9])+\.([01-9]){4}/';
+	$regular2='/([01-9]){1}/';	
 //первый if на пустое
-if(@$postdannyy['date']!='' and @$postdannyy['matematics']!='' and @$postdannyy['matematics']!='' and @$postdannyy['history']!='' )
-{
+		$regulardata=preg_match($regular1, $postinformation['date']);
+		$regularmatan=preg_match($regular2, $postinformation['date']);
+		$regularruss=preg_match($regular2, $postinformation['date']);
+		$regularhistory=preg_match($regular2, $postinformation['date']);
 
-//втрой if на длину и необходимые точки в date
-	if($postdannyy['date'][2]=='.' and $postdannyy['date'][5]=='.' and $dlinadata<=10 and $dlinamatan==1 and $dlinaruss==1 and $dlinahist==1 )
+	if($regulardata==1 and $regularmatan==1 and $regularruss==1 and $regularhistory==1 and $lengthdata==10 and $lengthhist==1 and $lengthruss==1 and $lengthmatan==1)
 	{
-//третий if проверка на числа
-		if(is_numeric($postdannyy['date'][0])==1 and is_numeric($postdannyy['date'][1])==1 and is_numeric($postdannyy['date'][3])==1 and is_numeric($postdannyy['date'][4])==1 and is_numeric($postdannyy['date'][6])==1 and is_numeric($postdannyy['date'][7])==1 and is_numeric($postdannyy['date'][8])==1 and is_numeric($postdannyy['date'][9])==1 and is_numeric($postdannyy['matematics'][0])==1 and is_numeric($postdannyy['russich'][0])==1 and is_numeric($postdannyy['history'][0])==1)
-		{
-
-
-//добавление sql запросом в базу данных
-$sql = 'INSERT INTO class(date,matematics,russich,history) VALUES(:date,:matematics,:russich,:history)';
-$params= ['date' => @$postdannyy['date'], 'matematics' => @$postdannyy['matematics'], 'russich'=>@$postdannyy['russich'],'history'=>@$postdannyy['history']];
-$stmt=$pdo->prepare($sql);
-$stmt->execute($params);
-}
+		//добавление sql запросом в базу данных
+		$sql = 'INSERT INTO class(date,matematics,russich,history) VALUES(:date,:matematics,:russich,:history)';
+		$params= ['date' => @$postinformation['date'], 'matematics' => @$postinformation['matematics'], 'russich'=>@$postinformation['russich'],'history'=>@$postinformation['history']];
+		$stmt=$pdo->prepare($sql);
+		$stmt->execute($params);
 	}
-		}
- header("Location: /laba/lcab.php");
+ 	header("Location: /laba/lcab.php");
+
+
+
+	
+
+
+
 
 ?>
